7c1ed8a28cf718257f41957482c90b8ec405f798
Glitch (thirsty-cheddar-pipe) <none>
2020-04-30 14:13:04 +0000

    🐙🎓 Checkpoint ./server.js:15407664/219

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/api/hello", function (req, res) {
 
 
 app.get("/api/whoami", function(req,res){
-  res.json({"ipaddress":req.ip, "language": req.language req.headers["accept-language"]})
+  res.json({"ipaddress":req.ip, "language": req.headers["accept-language"], "software":req.headers["user-agent"]})
 })
 
 
